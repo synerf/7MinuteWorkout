24522b4b8cb39b17c320742dc435c761a00fb82e
Ashish Pimpre <50199708+synerf@users.noreply.github.com>
2021-12-15 16:13:17 +0530

    add history activity, setup recyclerview for history data

--- app/src/main/java/com/synerf/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/synerf/a7minuteworkout/HistoryActivity.kt
@@ -2,10 +2,10 @@ package com.synerf.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.synerf.a7minuteworkout.databinding.ActivityHistoryBinding
-import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -37,8 +37,28 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao) {
         lifecycleScope.launch {
             historyDao.fetchAllDates().collect { allCompletedDatesList ->
-                for (i in allCompletedDatesList) {
-                    Log.e("Date: ", "" + i)
+                if(allCompletedDatesList.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(
+                        this@HistoryActivity, LinearLayoutManager.VERTICAL, true
+                    )
+
+                    val dates = ArrayList<String>()
+                    for(date in allCompletedDatesList) {
+                        dates.add(date.date)
+                    }
+
+                    val historyAdapter = HistoryAdapter(dates)
+
+                    binding?.rvHistory?.adapter = historyAdapter
+
+                } else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
                 }
             }
         }
